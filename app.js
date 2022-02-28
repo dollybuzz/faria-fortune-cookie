6844dc1d8a605c51dc6cb4250e0295b4d5ebfc8f
Dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2022-02-27 20:58:55 -1000

    fixed database connection - root user needed to be altered https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require("express");
+const mysql = require('mysql');
+
 const app = express();
 
+//Create database connection
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: "fortunecookie"
+});
+
+conn.connect((err)=> {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log("Connection established");
+    return conn;
+});
+
 //routes
+//root route
 app.get("/", function(req,res){
-    res.send("it works");
+    res.send("it works!");
 });
 
 //server listener
-app.listen("8080", "127.0.0.1", function (){
+app.listen("3307", "127.0.0.1", function (){
     console.log("express server running...");
 });
